c60720ca4bb669cffc31ee0aedcd148b0ed318d5
flyingwr <renanferncmrc@gmail.com>
2025-05-26 10:09:49 -0300

    patch: rewrite buffer class

--- swfparser/reader.py
+++ swfparser/reader.py
@@ -1,75 +1,67 @@
 import struct
 
 class ByteReader:
-    def __init__(self, data: bytes):
-        self.buf = bytearray(data)
-        self.pos = 0
+	def __init__(self, data: bytes):
+		self.buf = bytearray(data)
+		self.pos = 0
 
-    def read_u8(self) -> int:
-        val = self.buf[self.pos]
-        self.pos += 1
-        return val
+	def read_u8(self) -> int:
+		val = self.buf[self.pos]
+		self.pos += 1
+		return val
 
-    def read_u16(self) -> int:
-        val = struct.unpack_from('<H', self.buf, self.pos)[0]
-        self.pos += 2
-        return val
-    
-    def read_s24(self) -> int:
-        val = int.from_bytes(self.read_bytes(3), "little")
-        return val
+	def read_u16(self) -> int:
+		val = struct.unpack_from('<H', self.buf, self.pos)[0]
+		self.pos += 2
+		return val
+	
+	def read_s24(self) -> int:
+		return int.from_bytes(self.read_bytes(3), "little")
 
-    def read_u32(self) -> int:
-        val = struct.unpack_from('<I', self.buf, self.pos)[0]
-        self.pos += 4
-        return val
+	def read_u32(self) -> int:
+		val = struct.unpack_from('<I', self.buf, self.pos)[0]
+		self.pos += 4
+		return val
 
-    def read_bytes(self, n: int) -> bytes:
-        b = self.buf[self.pos:self.pos + n]
-        self.pos += n
-        return b
+	def read_d(self) -> float:
+		val = struct.unpack_from('<d', self.buf, self.pos)[0]
+		self.pos += 8
+		return val
 
-    def read_leb128(self) -> int:
-        result = 0
-        shift = 0
+	def read_bytes(self, n: int) -> bytes:
+		b = self.buf[self.pos:self.pos + n]
+		self.pos += n
+		return b
 
-        while True:
-            byte = self.read_u8()
-            result |= (byte & 0x7F) << shift
+	def read_leb128(self) -> int:
+		result = 0
+		shift = 0
 
-            if not (byte & 0x80):
-                break
-            
-            shift += 7
+		while True:
+			byte = self.read_u8()
+			result |= (byte & 0x7F) << shift
 
-        return result
+			shift += 7
+			
+			if not (byte & 0x80) or shift == 35:
+				break
+			
+		return result
 
-    def read_sleb128(self) -> int: # S32
-        result = 0
-        shift = 0
+	def read_sleb128(self) -> int: # S32
+		result = self.read_leb128()
+		if result & (1 << 31):
+			result -= 1 << 32
+		return result
+	
+	def read_string(self) -> str:
+		length = self.read_leb128()
+		raw = self.read_bytes(length)
+		return raw.decode()
 
-        while True:
-            byte = self.read_u8()
-
-            result |= ((byte & 0x7F) << shift)
-            shift += 7
-
-            if not (byte & 0x80) or shift == 35:
-                break
-
-        if result & (1 << 31):
-            result -= 1 << 32
-
-        return result
-    
-    def read_string(self) -> str:
-        length = self.read_leb128()
-        raw = self.read_bytes(length)
-        return raw.decode()
-
-    def read_sstring(self) -> str:
-        start = self.pos
-        while self.buf[self.pos] != 0:
-            self.pos += 1
-        self.pos += 1
-        return self.buf[start:self.pos].decode().rstrip("\x00")
+	def read_sstring(self) -> str:
+		start = self.pos
+		while self.buf[self.pos] != 0:
+			self.pos += 1
+		self.pos += 1
+		return self.buf[start:self.pos].decode().rstrip("\x00")
--- swfparser/writer.py
+++ swfparser/writer.py
@@ -3,7 +3,9 @@ import struct
 class ByteWriter:
     def __init__(self):
         self.buf = bytearray()
-        self.pos = 0
+
+    def clear(self):
+        self.buf *= 0
 
     def write_u8(self, v: int) -> 'ByteWriter':
         self.buf.append(v & 0xFF)
@@ -21,6 +23,10 @@ class ByteWriter:
         self.buf.extend(struct.pack("<I", v & 0xFFFFFFFF))
         return self
 
+    def write_d(self, v: int) -> 'ByteWriter':
+        self.buf.extend(struct.pack("<d", v))
+        return self
+
     def write_bytes(self, b: bytes) -> 'ByteWriter':
         self.buf.extend(b)
         return self
@@ -33,20 +39,16 @@ class ByteWriter:
                 self.buf.append(byte)
                 break
             self.buf.append(byte | 0x80)
-
         return self
     
-    def write_sleb128(self, v: int) -> 'ByteWriter':
-        while True:
-            byte = v & 0x7F
-            v >>= 7
-
-            if (v == 0 and (byte & 0x40) == 0) or (v == -1 and (byte & 0x40) != 0):
-                self.buf.append(byte)
-                break
-            
-            self.buf.append(byte | 0x80)
-        return self
-    
+    def write_sleb128(self, v: int) -> 'ByteWriter': # S32
+        if v & (1 << 31):
+            v -= 1 << 32
+        return self.write_leb128(v)
+        
     def write_string(self, s: str) -> 'ByteWriter':
-        return self.write_leb128(len(s)).write_bytes(s.encode())
+        return self.write_leb128(len(s.encode())).write_bytes(s.encode())
+    
+    def write_sstring(self, s: str) -> 'ByteWriter':
+        self.buf.extend(s.encode() + b"\x00")
+        return self
